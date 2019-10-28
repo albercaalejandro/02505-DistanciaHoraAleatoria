a52e12118c7f5567b27bee75a009f39955841731
Xtek <Xtek@Pc-Nasa>
2019-10-28 17:59:34 +0100

    :metal: Commit resubido

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package distanciahoraaleatoria;
+package org.japo.java.main;
 import java.util.Calendar;
 import java.util.Random;
 /**
